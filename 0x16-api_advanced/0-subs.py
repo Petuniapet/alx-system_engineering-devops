cec73fe840580b2ab8a3f4d919a85d848147adbd
Petuniapet <petuniastemere@gmail.com>
2024-05-07 09:29:24 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+
+    Args:
+        subreddit: A string representing the subreddit name.
+
+    Returns:
+        The number of subscribers of the subreddit, or 0 if invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    pass
